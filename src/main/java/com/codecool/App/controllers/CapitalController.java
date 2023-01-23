a4cafb9be8d89bc3fd574cbabf6d4fc3d003ddf6
904756 <904756@gmail.com>
2023-01-24 00:49:41 +0200

    Add sprint1 to the repository

---
+++ src/main/java/com/codecool/App/controllers/CapitalController.java
@@ -0,0 +1,35 @@
+package com.codecool.App.controllers;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+public class CapitalController {
+
+    private final String apiKey = "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";
+    private final String endpoint = "https://api.openai.com/v1/engines/davinci/completions";
+
+    @PostMapping("/capital")
+    public String generateResponse(@RequestBody String prompt) throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(apiKey);
+
+        HttpEntity<String> request = new HttpEntity<>(prompt, headers);
+        ResponseEntity<String> response = restTemplate.exchange(endpoint, HttpMethod.POST, request, String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(response.getBody());
+        return jsonNode.get("choices").get(0).get("text").toString();
+    }
+}
+
+
+
