94bfbb9d62fd9a02eff684e66336d90ee5b64ea8
904756 <904756@gmail.com>
2023-01-24 10:34:51 +0200

    Add react app and a little navbar

---
+++ ai-teacher/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <div className="logo">W3Schools</div>
+        </nav>
+    );
+}
+
+export default Navbar;
