55d9570b0888a4e8223e3ea549af88f1e496a279
rosun <edglobalsolutions@gmail.com>
2023-01-24 15:45:09 +0200

    Header added

--- ai-teacher/src/App.js
+++ ai-teacher/src/App.js
@@ -6,11 +6,13 @@ import 'mdb-react-ui-kit/dist/css/mdb.rtl.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
         <Navbar />
+        <Header />
       <header className="App-header">
           <body>
           <h1>AI Teacher</h1>
