cd02f96975d397389a53e11f8485f0e960bee1bd
rosun <edglobalsolutions@gmail.com>
2023-01-26 18:16:46 +0200

    minor adjustments

--- ai-teacher/src/components/StartLearningPage.js
+++ ai-teacher/src/components/StartLearningPage.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import Math from './images/math.jpg';
+import phy from './images/phy.jpg';
 function StartLearningPage() {
 
     const [coursesNames, setCoursesNames] = useState("");
@@ -15,12 +16,12 @@ function StartLearningPage() {
 
     const courses = coursesNames.toString().split(",");
     const filteredCourses = courses.filter(course => course !== ""); // This will remove any empty spaces from the array
-    console.log(filteredCourses)
     const elements = filteredCourses.map((course, index) => (
-        <li key={course}>
-            {index+1}. {course}
+        <li key={course} className="d-flex justify-content-center align-items-center border border-secondary mb-3">
+            <img className="mr-3" src={course === "Math" ? `${Math}` : course === "Physics" ? `${phy}` : 'ai-teacher/src/components/images/math.jpg'} alt={course} width="50" height="50" />
+            <div> {index+1}. {course} </div>
             <Link to={course === "Math" ? '/MathAi' : course === "Physics" ? '/PhysicsAi' : `/`}>
-                <button>Go To AiTeacher</button>
+                <button className="ml-auto">Go To AiTeacher</button>
             </Link>
         </li>
     ));
