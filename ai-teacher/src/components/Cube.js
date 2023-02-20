6ac1fecd2abde41c8e81c0b77f16f2318c549208
904756 <904756@gmail.com>
2023-02-20 09:10:51 +0200

    Add content to About page, make main page presentable

---
+++ ai-teacher/src/components/Cube.js
@@ -0,0 +1,31 @@
+function Cube()  {
+
+    return (
+        <>
+            <div className="wrap">
+                <div className="cube">
+
+                    <div className="front">
+                        The Future of Education: How AI Teachers are Transforming the Learning Experience
+                    </div>
+                    <div className="back">
+                        A new AI way of learning
+                    </div>
+                    <div className="top">
+                        24/7 Access to a Personal Mentor
+                    </div>
+                    <div className="bottom">
+                        Personal Mentor and Teacher
+                    </div>
+                    <div className="left">
+                        Artificial Intelligence (AI).
+                    </div>
+                    <div className="right">
+                        LEARN SMARTER, NOT HARDER
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Cube;
