0775fecb3fb3c9371d4132a8a087295843950dd9
904756 <904756@gmail.com>
2023-02-21 14:46:19 +0200

    Small change on main page

--- ai-teacher/src/components/Body.js
+++ ai-teacher/src/components/Body.js
@@ -21,7 +21,7 @@ function Body()  {
                     <div className='d-flex justify-content-center align-items-center h-100'>
                         <div className='text-white'>
                             <h1 id= 'mainPageTitle' className='mb-3' style={{fontFamily: 'Gloria Hallelujah', fontSize: '100px'}}>AI TEACHER</h1>
-                            <h4 id= 'mainPageP1'className='mb-3' style={{fontFamily: 'Gloria Hallelujah', fontSize: '30px'}}>the future is digital</h4>
+                            <h4 id= 'mainPageP1'className='mb-3' style={{fontFamily: 'Gloria Hallelujah', fontSize: '30px'}}>- the future is digital -</h4>
                             <h5 id= 'mainPageP2'className='mb-3' style={{fontFamily: 'Gloria Hallelujah', fontSize: '40px'}}>start your learning NOW!</h5>
                         </div>
                     </div>
