a20085df0b32d28d769e5f0cbaf356278499904f
904756 <904756@gmail.com>
2023-02-21 13:34:36 +0200

    Change physics page

--- ai-teacher/src/components/PhysicsAiTeacher.js
+++ ai-teacher/src/components/PhysicsAiTeacher.js
@@ -54,112 +54,40 @@ function PhysicsAiTeacher() {
     useEffect(() => {
         fetchMessages();
     }, []);
+
     return (
-        <MDBRow>
+        <div>
             <Navbar />
-            <div
-                className="p-5 text-center bg-image"
-                style={{ backgroundImage: `url('${img}')`, height: "500px" }}
-            >
-                <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
-                    <div className="d-flex justify-content-center align-items-center h-100">
-                        <div className="text-white">
-                            <h1 className="mb-3">Physics AI Teacher</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <MDBCol sm="6">
-                <MDBCard>
-                    <MDBCardBody>
-                        <MDBCardTitle>Type your questions:</MDBCardTitle>
+            <div className="flex-container">
+                <aside className="sidemenu">
+                    <h6>You asked...</h6>
+                </aside>
+                <section className="question-container">
+                    <div id="aiTitle"><h1> Physics AI teacher </h1></div>
+                    <div><h4 id="aiQuote">"Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less" – Marie Curie</h4></div>
+                    <div className="chatbox">
                         <form onSubmit={handleSubmit}>
                             <div className="form-group">
-                                <label htmlFor="message"></label> <br />
-                                <textarea
-                                    id="message"
-                                    value={message}
-                                    onChange={(e) => setMessage(e.target.value)}
-                                    className="form-control border border-primary h-100"
-                                ></textarea>
+                            <textarea
+                                id="message"
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                rows="1" className="chat-input-textarea" placeholder="Type your question here">
+                            </textarea>
+                                <button type="submit" className="btn btn-outline-secondary ask">Ask</button>
+                                <div id="hint">Hint: you can ask for extra explanations</div>
                             </div>
-                            <MDBCardText>
-                                <br />
-                                Bring your questions to the AI teacher and get the answers.
-                            </MDBCardText>
-                            <button type="submit" className="btn btn-primary">
-                                Send
-                            </button>
                         </form>
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
-            <MDBCol sm="6">
-                <MDBRow className="h-100">
-                    <MDBCol className="d-flex flex-column">
-                        <MDBCard className="mb-3">
-                            <MDBCardBody>
-                                <MDBCardTitle>Teacher response : </MDBCardTitle>
-                                <div
-                                    className="d-flex justify-content-center align-items-center flex-grow-1"
-                                    style={{
-                                        fontSize: "1.5rem",
-                                        textAlign: "center",
-                                        backgroundColor: "aquamarine",
-                                        padding: "10px",
-                                        boxShadow: "10px 10px 8px #888888",
-                                    }}
-                                >
-                                    {response}
-                                </div>
-                                <MDBCardText>
-                                    <br />
-                                    Hint: you can ask for extra explanations.
-                                </MDBCardText>
-                            </MDBCardBody>
-                        </MDBCard>
-                        <MDBCard className="flex-grow-1">
-                            <MDBCardBody>
-                                <MDBCardTitle>Conversation Log </MDBCardTitle>
-                                {messages.length > 0 ? (
-                                    <div
-                                        className="d-flex flex-column justify-content-center align-items-center h-100"
-                                        style={{
-                                            fontSize: "1.5rem",
-                                            textAlign: "center",
-                                            backgroundColor: "aquamarine",
-                                            padding: "10px",
-                                            boxShadow: "10px 10px 8px #888888",
-                                            height: "500px", // set height of the div to make it scrollable
-                                            overflow: "scroll", // set overflow property to scroll
-                                        }}
-                                    >
-                                        {messages.map((message, index) => (
-                                            <div key={index}>
-                                                <p>
-                                                    <strong>Message:</strong> {message.prompt}
-                                                </p>
-                                                <p>
-                                                    <strong>Response:</strong> {message.response}
-                                                </p>
-                                            </div>
-                                        ))}
-                                    </div>
-                                ) : (
-                                    <div>No messages yet</div>
-                                )}
-                                <MDBCardText>
-                                    <br />
-                                    Scroll down to see the conversation log.
-                                </MDBCardText>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </MDBCol>
-                </MDBRow>
-            </MDBCol>
-            <Footer />
-        </MDBRow>
 
+                        <div className="chat-output-textarea">
+                            {response}
+                        </div>
+                    </div>
+
+                </section>
+            </div>
+            <Footer />
+        </div>
     );
 }
 
