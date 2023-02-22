9b686a18cfc32f864af992f9b6985595b7a90f00
rosun <edglobalsolutions@gmail.com>
2023-02-22 14:28:16 +0200

    Cube element created as react component and added to Teacher response in chat box instead of existing picture

--- ai-teacher/src/components/Cube.js
+++ ai-teacher/src/components/Cube.js
@@ -1,31 +1,20 @@
-function Cube()  {
+import React from "react";
 
+function Cube() {
     return (
         <>
             <div className="wrap">
                 <div className="cube">
-
-                    <div className="front">
-                        The Future of Education: How AI Teachers are Transforming the Learning Experience
-                    </div>
-                    <div className="back">
-                        A new AI way of learning
-                    </div>
-                    <div className="top">
-                        24/7 Access to a Personal Mentor
-                    </div>
-                    <div className="bottom">
-                        Personal Mentor and Teacher
-                    </div>
-                    <div className="left">
-                        Artificial Intelligence (AI).
-                    </div>
-                    <div className="right">
-                        LEARN SMARTER, NOT HARDER
-                    </div>
+                    <div className="front">AI</div>
+                    <div className="back">AI</div>
+                    <div className="top">AI</div>
+                    <div className="bottom">AI</div>
+                    <div className="left">AI</div>
+                    <div className="right">AI</div>
                 </div>
             </div>
         </>
     );
 }
+
 export default Cube;
