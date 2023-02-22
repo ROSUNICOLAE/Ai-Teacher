25711ec71c2696935532f493b372c296ce4ba290
904756 <904756@gmail.com>
2023-02-23 00:14:41 +0200

    Save Q&A on Math AI

--- ai-teacher/src/components/MathAiTeacher.js
+++ ai-teacher/src/components/MathAiTeacher.js
@@ -1,79 +1,145 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import img from './images/AI k12.png.jpg';
 import data from "bootstrap/js/src/dom/data";
+import jwt_decode from 'jwt-decode';
+import Cube from "./Cube";
 
 
 function MathAiTeacher() {
-    const [message, setMessage] = useState('');
-    const [response, setResponse] = useState('');
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [allMessages, setAllMessages] = useState([]);
+    const [username, setUsername] = useState("");
+
+
+    const fetchMessages = () => {
+        const requestOptions = {
+            method: "GET", headers: {
+                "Content-Type": "application/json", Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        };
+
+        fetch("http://localhost:8080/api/messages", requestOptions)
+            .then((response) => response.json())
+            .then((data) => setAllMessages(data));
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        fetch('http://localhost:8080/Mathai', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ prompt: message }),
-        })
-            .then(res => res.text())
-            .then(data => setResponse(data));
+        if (message.trim()) {
+            const newMessage = {
+                text: message,
+                isUser: true,
+                time: new Date().toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true})
+            };
+            const newMessages = [...messages, newMessage];
+            setMessages(newMessages);
+            const requestOptions = {
+                method: "POST", headers: {
+                    "Content-Type": "application/json", Authorization: `Bearer ${localStorage.getItem("token")}`,
+                }, body: JSON.stringify({prompt: message}),
+            };
+
+            fetch("http://localhost:8080/api/Mathai", requestOptions)
+                .then((response) => response.text())
+                .then((data) => {
+                    const newResponse = {
+                        text: data, isUser: false, time: new Date().toLocaleString("en-US", {
+                            hour: "numeric", minute: "numeric", hour12: true,
+                        }),
+                    };
+                    const updatedMessages = [...newMessages, newResponse];
+                    setMessages(updatedMessages);
+                    setAllMessages([...allMessages, newResponse]);
+                    fetchMessages();
+                });
+
+            setMessage("");
+        }
     };
-    return (
-        <div>
-            <Navbar />
+
+    useEffect(() => {
+        fetchMessages();
+    }, []);
+
+    useEffect(() => {
+        fetchMessages();
+        const token = localStorage.getItem("token");
+        console.log(token);
+        if (token) {
+            const decodedToken = jwt_decode(token);
+            console.log(decodedToken);
+            setUsername(decodedToken.sub);
+        }
+    }, []);
+
+    return (<div>
+            <Navbar/>
             <div className="flex-container">
-            <aside className="sidemenu">
-                <h6>You asked...</h6>
-            </aside>
-            <section className="question-container">
-                    <div id="aiTitle"><h1> Math AI teacher </h1></div>
-                    <div><h4 id="aiQuote">"Mathematics is the language in which God has written the universe." – Galileo Galilei</h4></div>
-                <div className="chatbox">
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <textarea
-                                      id="message"
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                                rows="1" className="chat-input-textarea" placeholder="Type your question here and hit enter">
-                            </textarea>
-                        <button type="submit" className="btn btn-outline-secondary ask">Ask</button>
-                            <div id="hint">Hint: you can ask for extra explanations</div>
-                        </div>
-                    </form>
-
-                    <div className="chat-output-textarea">
-                        {response}
+                <aside className="sidemenu" style={{overflowY: "auto"}}>
+                    <h6 className="side-menu-button">Asked questions</h6>
+                    {allMessages.map((message, index) => (<div key={index}>
+                        <div className="side-menu-button">
+                            <p><strong>Question:</strong> {message.prompt}</p>
+                            <p><strong>Response:</strong> {message.text || message.response}</p>
+                        </div><p></p>
+                    </div>))}
+                </aside>
+                <div className="question-container">
+                    <div id="aiTitle">
+                        <h1>Math AI teacher</h1>
                     </div>
-                </div>
-            </section>
-                <div className="wrap">
-                    <div className="cube">
-
-                        <div className="front writing">
-                            AI Teacher
-                        </div>
-                        <div className="back writing">
-                            Learn
-                        </div>
-                        <div className="top writing">
-                            24/7 Access
-                        </div>
-                        <div className="bottom writing">
-                            Mentor
-                        </div>
-                        <div className="left writing">
-                            SMARTER
-                        </div>
-                        <div className="right writing">
-                            NOT HARDER
+                    <div><h4 id="aiQuote">"Mathematics is the language in which God has written the universe." – Galileo Galilei</h4></div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <div className="maincontainer" style={{width: "50%"}}>
+                            <div
+                                id="msg-box"
+                                className="card-body msg_card_body"
+                                style={{height: "500px", overflowY: "auto"}}>
+                                {messages.map((msg, index) => (
+                                    <div
+                                        key={index}
+                                        className={msg.isUser ? "d-flex justify-content-end mb-4" : "d-flex justify-content-start mb-4"}>
+                                        <div className="img_cont_msg" style={{position: "relative"}}>
+                                            {msg.isUser ? (<span>{username.charAt(0).toUpperCase() + username.slice(1)}</span>)
+                                                : (
+                                                    <Cube style={{position: "relative", top: -300, left: 0, zIndex: -50}} />)}
+                                        </div>
+                                        <div
+                                            className={msg.isUser ? "msg_cotainer_send" : "msg_cotainer"}>
+                                            {msg.text}
+                                            <span className={msg.isUser ? "msg_time_send" : "msg_time"}>{msg.time}</span>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="card-footer">
+                                <form onSubmit={handleSubmit} className="input-group">
+                                    <label htmlFor="message"></label>
+                                    <br/>
+                                    <textarea
+                                        id="message"
+                                        value={message}
+                                        onChange={(e) => setMessage(e.target.value)}
+                                        name=""
+                                        className="form-control type_msg"
+                                        placeholder="Type your question here and hit enter"
+                                        onKeyPress={(e) => {
+                                            if (e.key === "Enter") {
+                                                handleSubmit(e);
+                                            }
+                                        }}
+                                    ></textarea>
+                                    <div className="input-group-append">
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <Footer />
+            <Footer/>
         </div>
     );
 }
